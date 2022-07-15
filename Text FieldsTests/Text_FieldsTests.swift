2078da79c6f40316af8658417acafc393b268d22
heyhocombo <heyhocombo@gmail.com>
2022-07-15 14:47:09 +0300

    Tests Refactor

--- Text FieldsTests/Text_FieldsTests.swift
+++ Text FieldsTests/Text_FieldsTests.swift
@@ -21,70 +21,48 @@ class Text_FieldsTests: XCTestCase {
     }
     
     // MARK: -  No Digits Input
-    func testNodidgitsField_IgnoreDidgits() throws {
-        //Given
-        let inputWithNoDidgits = "abcd"
-        let inputContainDidgits = "123asd"
-        //Then
-        XCTAssertTrue(sut.isValidNoDigitsString(userInput:inputWithNoDidgits))
-        XCTAssertFalse(sut.isValidNoDigitsString(userInput: inputContainDidgits))
-        
+    func testNoDigitsField_IgnoreDidgits() {
+        XCTAssertTrue(sut.isValidNoDigitsString(userInput: "abcd"))
+        XCTAssertFalse(sut.isValidNoDigitsString(userInput: "123asd"))
     }
     
     // MARK: - Letters-Numbers
-    func testOnlyCharactersField_IsAllowedCharacters() throws {
-        //Given
-        let allowedInput = "ABCDE-12345"
-        let notAllowedInput = "al420-ae228"
-        //Then
-        XCTAssertTrue(sut.isAllowedChar(text: allowedInput, replacementString: "0"))
-        XCTAssertFalse(sut.isAllowedChar(text: notAllowedInput, replacementString: "0"))
+    func testOnlyCharactersField_IsAllowedCharacters() {
+        XCTAssertTrue(sut.isAllowedChar(text: "ABCDE-12345", replacementString: "0"))
+        XCTAssertFalse(sut.isAllowedChar(text: "al420-ae228", replacementString: "0"))
+        XCTAssertFalse(sut.isAllowedChar(text: "ABCde-ABCDE", replacementString: "0"))
+        XCTAssertFalse(sut.isAllowedChar(text: "12345-54321", replacementString: "0"))
+        XCTAssertFalse(sut.isAllowedChar(text: "12345-ABCDE", replacementString: "0"))
     }
     
     // MARK: - Web Link
-    func testLinkTextField_URLvalidation() throws {
-        //Given
-        let correctlink = "https://www.apple.com"
-        let notCorrectLink = "apple"
-        //Then
-        XCTAssertEqual(sut.checkUrlValidation(input:notCorrectLink), "")
-        XCTAssertNotNil(sut.checkUrlValidation(input:correctlink))
+    func testLinkTextField_URLValidation() {
+        XCTAssertEqual(sut.checkUrlValidation(input: "apple"), "")
+        XCTAssertNotNil(sut.checkUrlValidation(input: "https://www.apple.com"))
     }
     
     // MARK: - Password walidation
-    func testValidationRulesTextField_IsMinOfCharacters() throws {
-        //Given
-        let input1 = "asdgasghasdfhgfdahdsfg"
-        let input2 = "123"
-        //Then
-        XCTAssertTrue(sut.hasRequiredQuantityOfCharacters(charCount: input1.count))
-        XCTAssertFalse(sut.hasRequiredQuantityOfCharacters(charCount: input2.count))
+    func testValidationRulesTextField_IsMinOfCharacters() {
+        XCTAssertTrue(sut.hasRequiredQuantityOfCharacters(charCount: "asdgasghasdfhgfdahdsfg".count))
+        XCTAssertFalse(sut.hasRequiredQuantityOfCharacters(charCount: "123".count))
+        XCTAssertTrue(sut.hasRequiredQuantityOfCharacters(charCount: "12345678".count))
+        XCTAssertFalse(sut.hasRequiredQuantityOfCharacters(charCount: "1234567".count))
     }
     
-    func testValidationRulesTextField_IsContainsDigit() throws {
-        //Given
-        let inputContainingDigit = "123abc"
-        let inputWithoutDigits = "abc"
-        //Then
-        XCTAssertTrue(sut.isContainingDigits(text: inputContainingDigit))
-        XCTAssertFalse(sut.isContainingDigits(text: inputWithoutDigits))
+    func testValidationRulesTextField_IsContainsDigit() {
+        XCTAssertTrue(sut.isContainingDigits(text: "123abc"))
+        XCTAssertFalse(sut.isContainingDigits(text: "abc"))
+        XCTAssertFalse(sut.isContainingDigits(text: "abcO"))
+        XCTAssertTrue(sut.isContainingDigits(text: "abc0"))
     }
     
-    func testValidationRulesTextField_IsContainsLowercaseCharacters() throws {
-        //Given
-        let inputContainingLowercaseChar = "abcd"
-        let inputWithoutLowercaseChar = "ABCD"
-        //Then
-        XCTAssertFalse(sut.isContainsLowercase(text: inputWithoutLowercaseChar))
-        XCTAssertTrue(sut.isContainsLowercase(text: inputContainingLowercaseChar))
+    func testValidationRulesTextField_IsContainsLowercaseCharacters() {
+        XCTAssertFalse(sut.isContainsLowercase(text: "ABCD"))
+        XCTAssertTrue(sut.isContainsLowercase(text: "abcd"))
     }
     
-    func testValidationRulesTextField_IsContainsUppercaseCharacters_Rule() throws {
-        //Given
-        let inputContainingUppercaseChar = "ABCD"
-        let inputWithoutUppercaseChar = "abcd"
-        //Then
-        XCTAssertFalse(sut.isContainsUppercase(text: inputWithoutUppercaseChar))
-        XCTAssertTrue(sut.isContainsUppercase(text: inputContainingUppercaseChar))
+    func testValidationRulesTextField_IsContainsUppercaseCharacters_Rule() {
+        XCTAssertFalse(sut.isContainsUppercase(text: "abcd"))
+        XCTAssertTrue(sut.isContainsUppercase(text: "ABCD"))
     }
 }
--- Text FieldsUITests/Text_FieldsUITests.swift
+++ Text FieldsUITests/Text_FieldsUITests.swift
@@ -9,69 +9,74 @@ import XCTest
 @testable import Text_Fields
 
 class Text_FieldsUITests: XCTestCase {
+    var app: XCUIApplication!
+    lazy var noDidgitsTextField = app.textFields.element(boundBy: 0)
+    lazy var inputLimitTextField = app.textFields.element(boundBy: 1)
+    lazy var onlyCharactersTextField = app.textFields.element(boundBy: 2)
+    lazy var linkTextField = app.textFields.element(boundBy: 3)
+    lazy var validationRulesTextField = app.textFields.element(boundBy: 4)
+    
+    override func setUpWithError() throws {
+        self.app = XCUIApplication()
+        self.app.launch()
+    }
+    
     // MARK: -  No Digits Input
-    func testNoDigitsTextField_Action() throws {
+    func testNoDigitsTextField_Action() {
+        //Given
         let input = "Alloha1 12b"
         let expectedOutput = "Alloha b"
-        let app = XCUIApplication()
-        let noDidgitsTextField = app.textFields.element(boundBy: 0)
-        app.launch()
+        //When
         noDidgitsTextField.tap()
         noDidgitsTextField.typeText(input)
+        //Then
         XCTAssertEqual((noDidgitsTextField.value as? String), expectedOutput)
     }
     
     // MARK: - Letters-Numbers
-    func testInputLimitFieldCounterAction() throws {
-        let app = XCUIApplication()
-        let inputLimitTextField = app.textFields.element(boundBy: 1)
-        app.launch()
+    func testInputLimitFieldCounterAction() {
+        //Given
+        let input = "Check input limit"
+        //When
         inputLimitTextField.tap()
-        inputLimitTextField.typeText("Check input limit")
+        inputLimitTextField.typeText(input)
+        //Then
         XCTAssertTrue(app.staticTexts["-7/10"].exists)
     }
     
     // MARK: - Letters-Numbers
-    func testOnlyCharactersFieldInteraction() throws {
-        let app = XCUIApplication()
-        let onlyCharactersTextField = app.textFields.element(boundBy: 2)
+    func testOnlyCharactersFieldInteraction() {
+        //Given
         let input = "A334sse3t-1k3ll4nj5l9"
         let expectedOutput = "Asset-13459"
-        app.launch()
+        //When
         onlyCharactersTextField.tap()
         onlyCharactersTextField.typeText(input)
+        //Then
         XCTAssertEqual((onlyCharactersTextField.value as? String), expectedOutput)
     }
     
     // MARK: - Web Link
-    func testWebLinkTextFieldInterAction() throws{
-        let app = XCUIApplication()
-        let linkTextField = app.textFields.element(boundBy: 3)
+    func testWebLinkTextFieldInterAction() {
+        //Given
         let input = "www.apple.com"
         let expectedOutput = "https://www.apple.com"
-        app.launch()
+        //When
         linkTextField.tap()
         linkTextField.typeText(input)
+        //Then
         XCTAssertEqual((linkTextField.value as? String), expectedOutput)
     }
     
     // MARK: - Password walidation
-    
-    func testValidationRulesFieldInteraction() throws {
-        let app = XCUIApplication()
-        let validationRulesTextField = app.textFields.element(boundBy: 4)
-        app.launch()
+    func testValidationRulesFieldInteraction() {
+        let input = "1aA45678"
         validationRulesTextField.tap()
-        // Check labels with validation rules.
-        validationRulesTextField.typeText("1")
-        XCTAssertTrue(app.staticTexts["✓ Min 1 digit"].exists)
-        validationRulesTextField.typeText("a")
-        XCTAssertTrue(app.staticTexts["✓ Min 1 lowercase"].exists)
-        validationRulesTextField.typeText("A")
-        XCTAssertTrue(app.staticTexts["✓ Min 1 capital required"].exists)
-        validationRulesTextField.typeText("abcde")
+        validationRulesTextField.typeText(input)
         app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(app.staticTexts["✓ Min 1 digit"].exists)
+        XCTAssertTrue(app.staticTexts["✓ Min 1 lowercase"].exists)
+        XCTAssertTrue(app.staticTexts["✓ Min 1 capital required"].exists)
         XCTAssertTrue(app.staticTexts["✓ Min lengh 8 characters"].exists)
     }
 }
-
